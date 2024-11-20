23a35fbb7e61cf1c534405cef352bb57c50a84ad
Mikaela Duran <mikaeladuran93@gmail.com>
2024-11-20 14:41:48 +0100

    Ändrat i ScoreWindow

--- src/client/ScoreWindow.java
+++ src/client/ScoreWindow.java
@@ -9,11 +9,12 @@ import java.awt.*;
 public class ScoreWindow extends JFrame {
 
     private Player player1, player2;
-    //private int rounds = 0;
+    private int rounds = 6;
 
     ScoreWindow(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.rounds = rounds;
         // this.rounds = PropertiesManager.totalRoundsSet();
         System.out.println(player1.getName() + " " + player2.getName());
 
@@ -78,9 +79,9 @@ public class ScoreWindow extends JFrame {
 
 
         //Mitt panelen med ronderna
-        JPanel rondPanel = new JPanel(new GridLayout(6, 1)); //TODO: 6 ändra till rounds
+        JPanel rondPanel = new JPanel(new GridLayout(rounds, 1)); //TODO: 6 ändra till rounds
         rondPanel.setOpaque(false);
-        for (int i = 1; i <= 6; i++) { //TODO: 6 ändra till rounds
+        for (int i = 1; i <= rounds; i++) { //TODO: 6 ändra till rounds
             // 7 kolumner motsvarar: 3 knappar + RondNr + 3 Knappar
             JPanel rowPanel = new JPanel(new GridLayout(1, 7));
             rowPanel.setOpaque(false);
@@ -95,7 +96,7 @@ public class ScoreWindow extends JFrame {
             }
 
             //Rond
-            JLabel rondLabel = new JLabel("Rond" + i, SwingConstants.CENTER);
+            JLabel rondLabel = new JLabel("Rond " + i, SwingConstants.CENTER);
             rondLabel.setFont(new Font("Arial", Font.BOLD, 14));
             rowPanel.add(rondLabel);
 
@@ -114,10 +115,10 @@ public class ScoreWindow extends JFrame {
         JPanel bottomPanel = new JPanel(new GridLayout(1, 2));
         bottomPanel.setOpaque(false);
         JButton giveUpButton = new JButton("Ge upp");
-        JButton waitButton = new JButton("Vänta");
-        waitButton.setEnabled(false);
+        JButton nextRoundButton = new JButton("Nästa rond");
+        nextRoundButton.setEnabled(false);
         bottomPanel.add(giveUpButton);
-        bottomPanel.add(waitButton);
+        bottomPanel.add(nextRoundButton);
 
 
         panel.add(topPanel, BorderLayout.NORTH);
@@ -125,12 +126,11 @@ public class ScoreWindow extends JFrame {
         panel.add(bottomPanel, BorderLayout.SOUTH);
         setVisible(true);
     }
-}
-    /*public static void main(String[] args) {
+
+    public static void main(String[] args) {
         //TODO: OBS HÅRDKODAT. Dessa ska man få från StartWindow
         Player player1 = new Player("Spelare 1",new ImageIcon("src/resources/avatars/Gengar.png"));
         Player player2 = new Player("Spelare 2",new ImageIcon("src/resources/avatars/Poliwag.png"));
-        ScoreWindow scoreWindow = new ScoreWindow( player1,  player2);/*
+        ScoreWindow scoreWindow = new ScoreWindow( player1,  player2);
     }
 }
-     */
