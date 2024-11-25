c06e6291eebdf82d24d3e489455a135a7a5624e0
Matilda Erenius <matildaerenius@hotmail.com>
2024-11-25 20:39:18 +0100

    la in bilder i knapparna i MenuWindow

--- src/client/gui/windows/MenuWindow.java
+++ src/client/gui/windows/MenuWindow.java
@@ -38,13 +38,21 @@ public class MenuWindow extends JFrame {
 
         // Knapp 1 - Slumpad spelare
         JButton button1 = new JButton("Slumpad spelare");
-        button1.setFont(new Font("Arial", Font.BOLD, 14));
-        button1.setPreferredSize(new Dimension(150, 100));
-        button1.setBackground(new Color(109, 187, 107));
+        button1.setFont(new Font("Arial", Font.BOLD, 16));
+        button1.setPreferredSize(new Dimension(160, 120));
+        button1.setBackground(new Color(25, 210, 139)); // TILLFÄLLIG FÄRG KANSKE
         button1.setForeground(Color.WHITE);
         button1.setBorder(BorderFactory.createLineBorder(Color.WHITE, 2));
         button1.setFocusable(false);
         button1.addActionListener(e -> windowManager.getNetworkHandler().startRandomGame(currentPlayer));
+
+        // Lägger till bild i knappen
+        ImageIcon randomIcon = new ImageIcon("src/resources/images/slumpad.png");
+        button1.setIcon(randomIcon);
+
+        // Placerar bilden ovanför texten
+        button1.setHorizontalTextPosition(SwingConstants.CENTER);
+        button1.setVerticalTextPosition(SwingConstants.BOTTOM);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
@@ -53,12 +61,20 @@ public class MenuWindow extends JFrame {
 
         // Knapp 2 - Spela mot en vän
         JButton button2 = new JButton("Spela mot en vän");
-        button2.setFont(new Font("Arial", Font.BOLD, 14));
-        button2.setPreferredSize(new Dimension(150, 100));
-        button2.setBackground(new Color(80, 127, 194));
+        button2.setFont(new Font("Arial", Font.BOLD, 16));
+        button2.setPreferredSize(new Dimension(160, 120));
+        button2.setBackground(new Color(72, 159, 254)); // TILLFÄLLIG FÄRG KANSKE
         button2.setForeground(Color.WHITE);
         button2.setBorder(BorderFactory.createLineBorder(Color.WHITE, 2));
         button2.setFocusable(false);
+
+        // Lägger till bild i knappen
+        ImageIcon friendIcon = new ImageIcon("src/resources/images/vän.png");
+        button2.setIcon(friendIcon);
+
+        // Placerar bilden ovanför texten
+        button2.setHorizontalTextPosition(SwingConstants.CENTER);
+        button2.setVerticalTextPosition(SwingConstants.BOTTOM);
 
         gbc.gridx = 1;
         gbc.gridy = 1;
@@ -68,7 +84,7 @@ public class MenuWindow extends JFrame {
         JButton button3 = new JButton("Topplista");
         button3.setFont(new Font("Arial", Font.BOLD, 16));
         button3.setPreferredSize(new Dimension(200, 50));
-        button3.setBackground(new Color(96, 140, 203));
+        button3.setBackground(new Color(67, 120, 189)); // TILLFÄLLIG FÄRG KANSKE
         button3.setForeground(Color.WHITE);
         button3.setBorder(BorderFactory.createLineBorder(Color.WHITE, 2));
         button3.setFocusable(false);
